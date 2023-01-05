6bd323721180b46dd7e8a106023bbbdbec2e5f1d
Brayan <brayankurahara@hotmail.com>
2023-01-05 14:15:13 -0300

    coloquei tristeza

---
+++ JS/scripts.js
@@ -0,0 +1,57 @@
+// Seleção de Elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+
+const cleanerBtn = document.querySelector("#cleaner-table");
+
+// Funções
+
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `<div class="row">
+    <div class="operation">${number} x ${i} = </div>
+    <div class="result">${result}</div>
+    </div>`;
+
+    const parser = new DOMParser();
+
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+
+    const row = htmlTemplate.querySelector(".row");
+
+    multiplicationTable.appendChild(row);
+  }
+
+  multiplicationTitle.innerText = number;
+};
+
+// Eventos
+
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  createTable(multiplicationNumber, multiplicatorNumber);
+});
+
+cleanerBtn.addEventListener("click", () => {
+  multiplicationTable.innerHTML = "";
+  multiplicationTitle.innerHTML = "";
+  return multiplicationTable;
+});
